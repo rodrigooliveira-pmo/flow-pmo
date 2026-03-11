ebaeac47bc5dfa6fcf3dfd38b7732ec253ffaf0d
Rodrigo Oliveira <rodrigo.oliveira@w1consultoria.com.br>
2026-03-11 10:44:13 -0300

    update vercel

--- node_modules/vercel/dist/help.js
+++ node_modules/vercel/dist/help.js
@@ -6,7 +6,7 @@ const __filename = __fileURLToPath(import.meta.url);
 const __dirname = __dirname_(__filename);
 import {
   help
-} from "./chunks/chunk-PPYHDRPY.js";
+} from "./chunks/chunk-7YHZDJ4G.js";
 import "./chunks/chunk-S7KYDPEM.js";
 import "./chunks/chunk-TZ2YI2VH.js";
 export {
